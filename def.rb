01d4f80713ea347d44b4847c873d0360c9a82585
Victor <victorsants360@gmail.com>
2016-07-03 10:57:17 -0300

    def

---
+++ def.rb
@@ -0,0 +1,8 @@
+def name (my_name, last_name)
+  puts "meu nome é #{my_name}"
+  puts "meu sobrenome é #{last_name}"
+end
+
+name("Thor", "Trovão")
+name("Odin", "Pai de Todos")
+name("Loki", "Trapaceiro")
